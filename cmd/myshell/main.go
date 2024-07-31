d587d6aacb6f9a40bcd739710fbf158f3b3c81a2
Ali Yasir Naç <aliyasirnac@gmail.com>
2024-07-31 11:37:45 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,12 +10,17 @@ import (
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-
-		s, err := bufio.NewReader(os.Stdout).ReadBytes('\n')
+		s, err := bufio.NewReader(os.Stdout).ReadString('\n')
 
 		if err != nil {
+			fmt.Println("panic")
+			os.Exit(1)
 		}
-
+		s = strings.TrimSpace(s)
+		if strings.Contains(s, "exit 0") {
+			os.Exit(0)
+		}
 		fmt.Fprint(os.Stdout, strings.TrimSpace(string(s))+": command not found\n")
+
 	}
 }
