b6bb368d287e2a8168b337fdb1a0e58058098e98
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-07-31 12:46:04 +0300

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,7 +7,18 @@ import (
 	"strings"
 )
 
+type Command struct {
+	name string
+}
+
+var commands = []Command{
+	{"type"},
+	{"exit"},
+	{"echo"},
+}
+
 func main() {
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 		s, err := bufio.NewReader(os.Stdout).ReadString('\n')
@@ -17,11 +28,29 @@ func main() {
 			os.Exit(1)
 		}
 		s = strings.TrimSpace(s)
+		if strings.HasPrefix(s, "type ") { // type command takes an argument so we need to check string with HasPrefix command
+			parts := strings.Split(s, " ")
+
+			cmd := strings.TrimSpace(parts[1])
+			found := false
+			for _, v := range commands {
+				if strings.Contains(v.name, cmd) {
+					fmt.Fprint(os.Stdout, cmd+" is a shell builtin\n")
+					found = true
+					break
+				}
+			}
+			if !found {
+				fmt.Fprint(os.Stdout, cmd+": not found\n")
+			}
+			continue
+		}
+
 		if strings.Contains(s, "exit 0") {
 			os.Exit(0)
 		}
 
-		if strings.Contains(s, "echo") {
+		if strings.HasPrefix(s, "echo") {
 			message := strings.Split(s, "echo ")
 
 			fmt.Fprint(os.Stdin, strings.Join(message, "")+"\n")
